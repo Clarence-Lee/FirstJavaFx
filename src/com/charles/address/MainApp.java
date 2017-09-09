16deb1b2cf2e8687564b1f02e15f88e10a66bec8
Clarence <limao693@sina.com>
2017-09-09 13:53:25 +0800

    add core methods

--- src/com/charles/address/MainApp.java
+++ src/com/charles/address/MainApp.java
@@ -1,8 +1,13 @@
 package com.charles.address;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 /**
  * Created by Administrator on 2017/9/9.
@@ -19,10 +24,46 @@ public class MainApp extends Application{
 
         initRootLayout();
 
-        ShowPersonOverview();
+        showPersonOverview();
     }
 
     public void initRootLayout() {
+        try {
+            //load root layout from builded fxml
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/RootLayout.fxml"));
+            rootLayout = (BorderPane) loader.load();
 
+            //show the scene containing the root layout
+            Scene scene = new Scene(rootLayout);
+            primaryStage.setScene(scene);
+            primaryStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+
+    public void showPersonOverview() {
+        try {
+            //load person overview
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(MainApp.class.getResource("view/PersonOverview.fxml"));
+            AnchorPane personOverview = (AnchorPane) loader.load();
+
+            //set person overview into the center of the root layout
+            rootLayout.setCenter(personOverview);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+
 }
