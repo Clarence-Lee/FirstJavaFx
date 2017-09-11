b9bdb3820f1336ddc9e94ae79088354f2cb9ce6f
Clarence <limao693@sina.com>
2017-09-11 16:37:59 +0800

    replace List，use ListWrapper

--- src/com/charles/address/model/PersonListWrapper.java
+++ src/com/charles/address/model/PersonListWrapper.java
@@ -1,16 +1,20 @@
 package com.charles.address.model;
 
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.util.List;
 
 /**
  * Created by Administrator on 2017/9/11.
  * Helper class to save list of person. This is used for saving the list of person to XML
  */
+@XmlRootElement
+// 定义根元素名称
 public class PersonListWrapper {
     private List<Person> persons;
 
     @XmlElement
+    //一个可选的名称，用来指定元素
     public List<Person> getPersons() {
         return persons;
     }
