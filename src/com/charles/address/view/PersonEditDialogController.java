0e943b795d19e5862eb99fd0c1169d51e0a2e3cb
Clarence <limao693@sina.com>
2017-09-11 12:13:36 +0800

    add Person model class

--- src/com/charles/address/view/PersonEditDialogController.java
+++ src/com/charles/address/view/PersonEditDialogController.java
@@ -3,6 +3,7 @@ package com.charles.address.view;
 import com.charles.address.Util.DateUtil;
 import com.charles.address.model.Person;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
@@ -96,7 +97,47 @@ public class PersonEditDialogController {
 
     private boolean isInputValid() {
         String errorMessage = "";
+        if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
+            errorMessage += "No valid first name!\n";
+        }
+        if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
+            errorMessage += "No valid last name!\n";
+        }
+        if (streetField.getText() == null || streetField.getText().length() == 0) {
+            errorMessage += "No valid street!\n";
+        }
+        if (postalCodeField.getText() == null || postalCodeField.getText().length() == 0){
+            errorMessage += "No valid postal code!\n";
+        } else {
+            try {
+                Integer.parseInt(postalCodeField.getText());
+            }catch (NumberFormatException e) {
+                errorMessage += "No valid postal code (must be an integer)!\n";
+            }
+        }
+        if (cityField.getText() == null || cityField.getText().length() == 0) {
+            errorMessage += "No valid city!\n";
+        }
 
+        if (birthdayField.getText() == null || birthdayField.getText().length() == 0) {
+            errorMessage += "No valid birthday!\n";
+        } else {
+            if (!DateUtil.validDate(birthdayField.getText())) {
+                errorMessage += "No valid birthday. Use the format dd.mm.yyyy!\n";
+            }
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Invalide Fields");
+            alert.setHeaderText("Please correct invalid fields");
+            alert.setContentText(errorMessage);
+
+            alert.showAndWait();
+            return false;
+        }
     }
 
 
