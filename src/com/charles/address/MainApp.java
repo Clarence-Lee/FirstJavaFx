9728a3c88483ca1c0335ef42286cc3dfd0fd06f2
Clarence <limao693@sina.com>
2017-09-11 15:41:17 +0800

    add Perferences

--- src/com/charles/address/MainApp.java
+++ src/com/charles/address/MainApp.java
@@ -14,7 +14,9 @@ import javafx.stage.Modality;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.prefs.Preferences;
 
 /**
  * Created by Administrator on 2017/9/9.
@@ -140,5 +142,39 @@ public class MainApp extends Application{
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    /*
+    Return the person file preference, i.e. the file that was opened.
+    The preference is read from the OS specific registry. if no one return null.
+     */
+
+    public File getPersonFilePath() {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        String filePath = prefs.get("filePath", null);
+        if (filePath != null) {
+            return new File(filePath);
+        } else {
+            return null;
+        }
+    }
+
+    /*
+    Sets the file path of the currectly loaded file.
+    @param file the file or null to remove the path
+     */
+    public void setPersonFilePath(File file) {
+        Preferences prefs = Preferences.userNodeForPackage(MainApp.class);
+        if (file != null) {
+            prefs.put("filePath", file.getPath());
+
+            //Update the stage title.
+            primaryStage.setTitle("AddressApp" + file.getName());
+        } else {
+            prefs.remove("filePath");
+
+            //Update the stage title.
+            primaryStage.setTitle("AddressApp");
+        }
     }
 }
